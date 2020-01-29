a7412e55274458e19a7661a50461530fd11fb65f
yunke zhu <yunke.zhu@tufts.edu>
2020-01-28 19:00:10 -0500

    completed, details are written in COLLABORATORS.txt

--- hw01/src/hw1.py
+++ hw01/src/hw1.py
@@ -1,107 +1,39 @@
-'''
-hw1.py
-Author: TODO
-
-Tufts COMP 135 Intro ML
-
-'''
+#!/usr/bin/env python
 
 import numpy as np
-
+import math
 def split_into_train_and_test(x_all_LF, frac_test=0.5, random_state=None):
-    ''' Divide provided array into train and test set along first dimension
-
-    User can provide a random number generator object to ensure reproducibility.
-
-    Args
-    ----
-    x_all_LF : 2D array, shape = (n_total_examples, n_features) (L, F)
-        Each row is a feature vector
-    frac_test : float, fraction between 0 and 1
-        Indicates fraction of all L examples to allocate to the "test" set
-    random_state : np.random.RandomState instance or integer or None
-        If int, code will create RandomState instance with provided value as seed
-        If None, defaults to the current numpy random number generator np.random
-
-    Returns
-    -------
-    x_train_MF : 2D array, shape = (n_train_examples, n_features) (M, F)
-        Each row is a feature vector
-        Should be a separately allocated array, NOT a view of any input array
-
-    x_test_NF : 2D array, shape = (n_test_examples, n_features) (N, F)
-        Each row is a feature vector
-        Should be a separately allocated array, NOT a view of any input array
-
-    Post Condition
-    --------------
-    This function should be side-effect free. The provided input array x_all_LF
-    should not change at all (not be shuffled, etc.)
-
-    Examples
-    --------
-    '''
-    np.random.seed(random_state)
-
-    x_all_LF
-
-
-
-
-    >>> x_LF = np.eye(10)
-    >>> xcopy_LF = x_LF.copy() # preserve what input was before the call
-    >>> train_MF, test_NF = split_into_train_and_test(
-    ...     x_LF, frac_test=0.3, random_state=np.random.RandomState(0))
-    >>> train_MF.shape
-    (7, 10)
-    >>> test_NF.shape
-    (3, 10)
-    >>> print(train_MF)
-    [[0. 0. 1. 0. 0. 0. 0. 0. 0. 0.]
-     [0. 0. 0. 0. 0. 0. 0. 0. 1. 0.]
-     [0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]
-     [0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]
-     [0. 1. 0. 0. 0. 0. 0. 0. 0. 0.]
-     [0. 0. 0. 0. 0. 0. 1. 0. 0. 0.]
-     [0. 0. 0. 0. 0. 0. 0. 1. 0. 0.]]
-    >>> print(test_NF)
-    [[0. 0. 0. 1. 0. 0. 0. 0. 0. 0.]
-     [1. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-     [0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]]
-
-    ## Verify that input array did not change due to function call
-    >>> np.allclose(x_LF, xcopy_LF)
-    True
-
-    References
-    ----------
-    For more about RandomState, see:
-    https://stackoverflow.com/questions/28064634/random-state-pseudo-random-numberin-scikit-learn
-    '''
+    tmp = x_all_LF.copy()
+    train_len = math.floor(len(tmp) * (1-frac_test))
     if random_state is None:
-        random_state = np.random
-    ## TODO fixme
-    return None, None
-
+        np.random.shuffle(tmp)
+    elif isinstance(random_state,int):
+        np.random.RandomState(random_state).shuffle(tmp)
+    else:
+        random_state.shuffle(tmp)
+    return tmp[:train_len], tmp[train_len:]
 
 def calc_k_nearest_neighbors(data_NF, query_QF, K=1):
-    ''' Compute and return k-nearest neighbors under Euclidean distance
+    l = []
+    for v2 in query_QF:
+        d = {}
+        tmp = []
+        for v1 in data_NF:
+            dist = distance(v1,v2)
+            if dist in d:
+                d[dist].append(v1)
+            else:
+                d[dist] = [v1]
+        for i in range(K):
+            cur = min(d.keys())
+            tmp.append(d[cur].pop())
+            if len(d[cur]) == 0:
+                d.pop(cur)
+        l.append(tmp)
+    return np.array(l)
 
-    Any ties in distance may be broken arbitrarily.
-
-    Args
-    ----
-    data_NF : 2D array, shape = (n_examples, n_features) aka (N, F)
-        Each row is a feature vector for one example in dataset
-    query_QF : 2D array, shape = (n_queries, n_features) aka (Q, F)
-        Each row is a feature vector whose neighbors we want to find
-    K : int, positive (must be >= 1)
-        Number of neighbors to find per query vector
-
-    Returns
-    -------
-    neighb_QKF : 3D array, (n_queries, n_neighbors, n_feats) (Q, K, F)
-        Entry q,k is feature vector of the k-th neighbor of the q-th query
-    '''
-    # TODO fixme
-    return None
+def distance(v1,v2):
+    sum = 0
+    for i in range(len(v1)):
+        sum += (v1[i] - v2[i]) * (v1[i] - v2[i])
+    return math.sqrt(sum)
